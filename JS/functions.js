d676de70ac2d65705452fe7e2b3b1b2072f81179
YesMyTomaTTich <moumengamer18@gmail.com>
2026-01-29 14:04:20 +0100

    funky font

--- JS/functions.js
+++ JS/functions.js
@@ -4,7 +4,7 @@ export function hueRotate(title) {
     setInterval(() => {
         hue += 1;
         title.style.filter = `hue-rotate(${hue}deg)`
-        if (hue == 360) {
+        if (hue === 360) {
             hue = 0;
         } 
     }, 90)
